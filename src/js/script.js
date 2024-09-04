fa8ad276b4d1422767b3b7769d307bf6cc1ba657
Vaibhav Chandrashekhar <vchandrashekhar@webmd.net>
2024-09-04 17:04:18 +0530

    Add url converter script

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,24 @@
 'use strict';
 function searchVideo() {
-    const query = document.getElementById('searchQuery').value;
+    let query = document.getElementById('searchQuery').value;
     if (query) {
+        query = replaceQueryParams(query);
         const url = `https://webmd-a.akamaihd.net/delivery/${encodeURIComponent(query)}`;
         const videoFrame = document.getElementById('videoFrame');
         const videoPlayer = document.getElementById('videoPlayer');
         videoPlayer.src = url;
         videoFrame.style.display = 'block';
     }
+}
+
+function replaceQueryParams(url) {
+    // Regular expression to match the pattern ",4500k,2500k,1000k,750k,400k,"
+    const pattern = /,4500k,2500k,1000k,750k,400k,/;
+
+    // Replace the matched pattern with ",1000k,"
+    const newUrl = url.replace(pattern, '1000k');
+
+    return newUrl;
 }
 
 function resetSearch() {
